f9cddbd6018609bd91460c0dd0738299165a4350
Nick-roy <nroy08232@gmail.com>
2026-07-03 18:08:12 +0600

    type

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,46 @@
 // const country = 'Bangladesh'
 // console.log(country)
- const sentence = "I Love My Country too mu"
-console.log(sentence)
+//  const sentence = "I Love My Country too mu"
+// console.log(sentence)
+
+// let playerName 
+// console.log(playerName)
+// playerName = "Messi"
+// console.log(playerName)
+// playerName = 23
+// function mult(a:number,b:number){
+// return a * b
+// }
+// const result = mult(5 , 3)
+// console.log(result)
+
+// let fruit = ['Apple', 'Guava','Banana','chary']
+
+// fruit.push('Orange')
+// console.log(fruit)
+
+// let mixed = ['Ana', 4 , true]
+// mixed.push({
+//     name:'nick'
+// })
+// console.log(mixed)
+
+// let person = {
+//     name: 'Nick Chandra ',
+//     old: 23,
+//     height: 5.8
+// }
+// const info = person.name
+// console.log(info)
+
+// let a: string ;
+
+// let b : number;
+
+// a = 'nick'
+
+// b = 3
+
+let a : (string | number)[] = [] 
+
+a.push(4 , 'sumit')
